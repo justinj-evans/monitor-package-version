d43bf5d28d2656b92ad40b493201e2e76b8f7c82
justinj-evans <11je20@queensu.ca>
2024-04-24 08:50:38 -0400

    docker for main.py run

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+
+# Parameters
+# - ${{ inputs.upgrade }}
+# - ${{ inputs.downgrade }}
+# - ${{ inputs.new_package }}
+# - ${{ inputs.token }}
+# - ${{ github.repository }}
+# - ${{ github.sha }}
+# - ${{ github.event.before }}
+
+# Print out the current directory
+echo "Contents of the current directory:"
+ls -l
+
+# Print out specific folders
+echo "Contents of the app directory:"
+ls -l app/
+
+# Run python main file
+python app/src/main.py \
+    --upgrade "$1" \
+    --downgrade "$2" \
+    --new_package "$3"\
+    --token "$4" \
+    --repo "$5" \
+    --commit_sha "$6" \
+    --existing_sha "$7" \
+
